42bc920a4af87ac90b42369c50ee7a93c4dbd8f7
Adrian Calcinas <adrian@google.com>
2024-09-26 11:26:53 -0400

    feat(configuración): añadir configuración de base de datos y servicios
    - Se ha añadido la configuración de la cadena de conexión en appsettings.json.
    - Se ha configurado el contexto de la base de datos LibreriaContext en Program.cs.
    - Se han registrado los servicios y repositorios en el contenedor de dependencias en Program.cs.
    - Se ha añadido la configuración de Swagger para la documentación de la API.

--- Libreria.PresentationLayer/Program.cs
+++ Libreria.PresentationLayer/Program.cs
@@ -1,6 +1,26 @@
+using Libreria.BusinessLogicLayer.Servicios;
+using Libreria.BusinessLogicLayer.Servicios.Contracts;
+using Libreria.DataAccessLayer.DataContext;
+using Libreria.DataAccessLayer.Repositories;
+using Libreria.DataAccessLayer.Repositories.Contract;
+using Libreria.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<LibreriaContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("ConnectionString")
+));
+
+builder.Services.AddScoped<IGenericRepository<Promocione>, PromocionesRepository>();
+builder.Services.AddScoped<IGenericRepository<Proveedor>, ProveedorRepository>();
+
+
+builder.Services.AddScoped<IPromocionesService, PromocionesService>();
+builder.Services.AddScoped<IProveedorService, ProveedorService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,29 +36,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}

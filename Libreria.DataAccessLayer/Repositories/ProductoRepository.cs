15c8d6ac03b250ea31198c9d15f35f81b19f77bf
Adrian Calcinas <adrian@google.com>
2024-11-28 10:09:17 -0400

    fix(repositorios): incluir categoría en la consulta de productos en ProductoRepository

--- Libreria.DataAccessLayer/Repositories/ProductoRepository.cs
+++ Libreria.DataAccessLayer/Repositories/ProductoRepository.cs
@@ -62,7 +62,7 @@ public class ProductoRepository : IProductoRepository
     {
         try
         {
-            IQueryable<Producto> productos = _context.Productos;
+            IQueryable<Producto> productos = _context.Productos.Include(producto => producto.Categoria);
             return Task.FromResult(productos);
         }
         catch (Exception ex)
